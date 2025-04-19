326da912ad150ca7f39a6f7797142b81c1c028a6
Amaldev <amaldev.psn@gmail.com>
2025-04-19 16:58:10 +0530

    Hellow world using react-basic

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-/* 
-<div id="parent">
-    <div id="parent">
-        <h1 id="h1-tag" class="font-red">Hello World From React</h1>
-    </div>
-</div>
-*/
 
-const header = React.createElement("h1",
-    { id: "h1-tag", class: "font-red" },
-    "Hello World From React")
+const header = React.createElement(
+    "h1", { id: "parent", class: "font-red" }, "Hellow World")
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
