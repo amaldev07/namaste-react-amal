a0ec09acd7623df12450e11b4972f334fd2e33b3
Amaldev <amaldev.psn@gmail.com>
2025-04-19 17:05:41 +0530

    c

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 const header = React.createElement(
     "div", { id: "parent" },
     React.createElement("div", { id: "child" },
-        React.createElement("h1", { id: "h1-tag", class: "font-red" },
-            "I am an h1 tag")
+        [React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")]
     ))
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
